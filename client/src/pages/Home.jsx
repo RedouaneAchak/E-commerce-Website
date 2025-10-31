c4e48bc7274f29cbe0d1fa5f1e2e619e7014c040
Aounil <yassinaounil5@gmail.com>
2025-10-31 22:59:06 +0100

    adding some libraries and intializing folder structre

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/Home.css'
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Store</h1>
+      <p>Your home page content here</p>
+    </div>
+  )
+}
