f0e44fb9b31bb73e3dd8ad85413994b3aea9c552
Aounil <yassinaounil5@gmail.com>
2025-11-23 01:34:11 +0100

    testing card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,46 +1,36 @@
-import '../styles/ProductCard.css';
-import { FaShoppingCart, FaStar } from 'react-icons/fa';
+import "../styles/ProductCard.css";
 
 export default function ProductCard({ product }) {
   return (
-    <div className="product-card">
-      <div className="product-image">
-        <img src={product.image} alt={product.name} />
-        {product.discount && (
-          <span className="discount-badge">-{product.discount}%</span>
-        )}
-      </div>
-      
-      <div className="product-info">
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-category">{product.category}</p>
-        
-        <div className="product-rating">
-          {[...Array(5)].map((_, i) => (
-            <FaStar
-              key={i}
-              className={i < product.rating ? 'star-filled' : 'star-empty'}
-            />
-          ))}
-          <span className="rating-count">({product.reviews})</span>
-        </div>
-        
-        <div className="product-footer">
-          <div className="product-price">
-            {product.discount ? (
-              <>
-                <span className="price-original">${product.originalPrice}</span>
-                <span className="price-current">${product.price}</span>
-              </>
-            ) : (
-              <span className="price-current">${product.price}</span>
-            )}
+    <div className="el-wrapper">
+      <div className="box-up">
+        <img className="img" src={product.image} alt={product.name} />
+
+        <div className="img-info">
+          <div className="info-inner">
+            <span className="p-name">{product.name}</span>
+            <span className="p-company">{product.brand}</span>
           </div>
-          
-          <button className="add-to-cart-btn">
-            <FaShoppingCart />
-          </button>
+
+          {product.sizes && (
+            <div className="a-size">
+              Available sizes : <span className="size">{product.sizes.join(" , ")}</span>
+            </div>
+          )}
         </div>
+      </div>
+
+      <div className="box-down">
+        <div className="h-bg">
+          <div className="h-bg-inner"></div>
+        </div>
+
+        <button className="cart" onClick={() => console.log("Add to cart:", product)}>
+          <span className="price">${product.price}</span>
+          <span className="add-to-cart">
+            <span className="txt">Add in cart</span>
+          </span>
+        </button>
       </div>
     </div>
   );
